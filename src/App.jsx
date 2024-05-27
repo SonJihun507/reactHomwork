9f208da500612e16ac8a3d678d54b724eeb5d9f2
airtrader <hosi2714@naver.com>
2024-05-27 17:24:57 +0900

    react 1강 숙제

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
- 
+  
+  const [count, setCount] = useState(0);
+
+  
+  const addCount = () => {
+    setCount(count + 1);
+  };
+  
+  
+  const minusCount = () => {
+	  setCount(count - 1)
+  }
+
+  const resetCount = () => {
+    setCount(0);
+  }
+
   return (
-   <div></div>
-  )
+    <>
+      <div>
+        <button onClick={minusCount}>-</button>
+        
+        Count: {count}
+        
+        <button onClick={addCount}>+</button>
+        <button className="never-push" onClick={resetCount}>절대 누르고 싶어지는 빨간버튼</button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
