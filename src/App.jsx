b6a59e75c8e9334cc7ef717ba67422c4faf54b02
airtrader <hosi2714@naver.com>
2024-05-29 17:22:20 +0900

    2강 숙제

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,76 @@
-import "./App.css";
 import { useState } from "react";
 
 function App() {
-  
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
 
-  
-  const addCount = () => {
-    setCount(count + 1);
+  const inputChangeHandler = (e) => {
+    setInput(e.target.value);
   };
-  
-  
-  const minusCount = () => {
-	  setCount(count - 1)
-  }
 
-  const resetCount = () => {
-    setCount(0);
-  }
+  const [todo, setTodo] = useState([
+    {
+      id: Date.now(),
+      text: "잠자기",
+      completed: true,
+    }, 
+    {
+      id: Date.now(),
+      text: "일찍 일어나기",
+      completed: false,
+    }
+  ]);
+
+
+  const addTodlHandler = (e) => {
+    e.preventDefault();
+    if (!input.trim()/*앞 뒤 띄어쓰기 없애기*/) return;
+    const newTodo = {
+      id : Date.now(),
+      text : input,
+      completed: false,
+    };
+    setTodo([...todo, newTodo]);
+    setInput("");
+  };
+
+  const deleteTodoHandler = (id) => {
+    setTodo(todo.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodoHandler = (id) => {
+    setTodo(
+      todo.map((todo) => 
+        todo.id === id ? {... todo, completed : !todo.completed} : todo)
+    );
+  };
 
   return (
-    <>
-      <div>
-        <button onClick={minusCount}>-</button>
+    <div>
+      <h1>할 일 목록</h1>
+      <form onSubmit={addTodlHandler}>
+        <input 
+        type="text" 
+        placeholder="할 일을 추가하세요" 
+        value={input}
+        onChange={inputChangeHandler}/>
+        <button type="submit">추가</button>
+      </form>
+      <ul>
+        {todo.map((todo) => (
+          <li 
+          key={todo.id}
+          style={{ textDecoration: todo.completed ? "line-through" : "" }}>
+          {todo.text}
+          <button onClick={() => toggleTodoHandler(todo.id)}>
+            {todo.completed ? "취소" : "완료"}
+            </button>
+          <button onClick={() => deleteTodoHandler(todo.id)}>삭제</button>
+        </li>
+        ))}
         
-        Count: {count}
-        
-        <button onClick={addCount}>+</button>
-        <button className="never-push" onClick={resetCount}>절대 누르고 싶어지는 빨간버튼</button>
-      </div>
-    </>
+      </ul>
+    </div>
   );
-}
+};
 
 export default App;
